91a31bb6e28a8c018119e298ec467fc74b7fe413
Your Name <you@example.com>
2025-03-30 10:36:00 +0000

    ..

--- run.sh
+++ run.sh
@@ -86,6 +86,8 @@ $RAY_EXEC status
 SCRIPT="python3.10 database_project/src/deduplication_spark.py --input_file \"/dev/shm/c4_files/c4-train.*.json.gz\" --output /dev/shm/c4_outputs --use_ray True"
 
 
+SCRIPT="$SCRIPT --implementation tfidf_minhash"
+# SCRIPT="$SCRIPT  --num_perm 1024 --threshold 0.9"
 
 # Run only on head node
 if $IS_HEAD; then
@@ -93,7 +95,7 @@ if $IS_HEAD; then
     # for NUM_FILES in 1 5 10 20 30 40; do
     for NUM_FILES in 40; do
         # COMMAND="$SCRIPT --limit_files $NUM_FILES"
-        COMMAND="$SCRIPT --limit_files $NUM_FILES --num_perm 1024 --threshold 0.9"
+        COMMAND="$SCRIPT --limit_files $NUM_FILES"
         rm -rf /dev/shm/c4_outputs 
         mkdir -p /dev/shm/c4_outputs
         echo "$COMMAND"
