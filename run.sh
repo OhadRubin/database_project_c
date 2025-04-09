351a73b96feb5ecd12da157265232bd36dd3f802
Your Name <you@example.com>
2025-04-09 12:27:41 +0000

    ..

--- run.sh
+++ run.sh
@@ -121,18 +121,19 @@ $RAY_EXEC status
 
 
 SCRIPT="python3.10 database_project/src/deduplication_spark.py --input_file \"/dev/shm/c4_files/c4-train.*.json.gz\" --output /dev/shm/c4_outputs --use_ray True"
+SCRIPT="$SCRIPT --implementation tfidf_minhash_ray"
+
+
+SCRIPT="python3.10 database_project/src/new_deduplication_spark.py --workflow nd_cl --input_file \"/dev/shm/c4_files/c4-train.*.json.gz\" --output /dev/shm/c4_outputs --use_ray True"
 
 
 # SCRIPT="$SCRIPT --implementation tfidf_minhash"
-SCRIPT="$SCRIPT --implementation tfidf_minhash_ray"
 # SCRIPT="$SCRIPT  --num_perm 1024 --threshold 0.9"
 
 # Run only on head node
 if $IS_HEAD; then
-    # python3.10 database_project/src/deduplication_spark.py --input_file "/dev/shm/c4_files/c4-train.*.json.gz" --output /dev/shm/c4_outputs --use_ray True --limit_files 1
     # for NUM_FILES in 1 5 10 20 30 40; do
     for NUM_FILES in 40; do
-        # COMMAND="$SCRIPT --limit_files $NUM_FILES"
         COMMAND="$SCRIPT --limit_files $NUM_FILES"
         rm -rf /dev/shm/c4_outputs 
         mkdir -p /dev/shm/c4_outputs
@@ -144,10 +145,3 @@ else
 fi
 
 sleep 600
-# for NUM_FILES in 1 5 10 20 30 40; do
-#     COMMAND="$SCRIPT --limit_files $NUM_FILES"
-#     rm -rf /dev/shm/c4_outputs 
-#     mkdir -p /dev/shm/c4_outputs
-#     echo "$COMMAND"
-#     eval "$COMMAND"
-# done
